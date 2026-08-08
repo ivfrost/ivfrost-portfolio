c1698a014e5a4b6147200081e3622596fadfe51b
ivfrost <120138606+ivfrost@users.noreply.github.com>
2026-08-08 11:20:27 +0200

    chore: update about copy and stack order

--- data/stack.tsx
+++ data/stack.tsx
@@ -1,27 +1,25 @@
-import { StackItem } from './types';
-import { TbBrandNextjs, TbBrandTypescript } from 'react-icons/tb';
 import { FaNodeJs, FaReact } from 'react-icons/fa';
-import { SiSpring } from 'react-icons/si';
 import { LiaJava } from 'react-icons/lia';
+import { SiSpring } from 'react-icons/si';
+import { TbBrandNextjs, TbBrandTypescript } from 'react-icons/tb';
+import { StackItem } from './types';
 
 export const stack: StackItem[] = [
 	{
-		name: 'React',
+		name: 'Java',
 		icon: (
-			<FaReact
-				size={16}
-				strokeWidth={1.5}
-				className="inline-block text-text-meta-lite ml-1.25 -mt-0.5"
+			<LiaJava
+				size={21}
+				className="-mt-1 inline-block text-text-meta-lite ml-1"
 			/>
 		),
 	},
 	{
-		name: 'Next.js',
+		name: 'Spring',
 		icon: (
-			<TbBrandNextjs
-				size={18}
-				strokeWidth={1.5}
-				className="inline-block text-text-meta-lite ml-1.25 -mt-0.5"
+			<SiSpring
+				size={13.5}
+				className="inline-block text-text-meta-lite ml-1.5 -mt-0.5"
 			/>
 		),
 	},
@@ -46,20 +44,22 @@ export const stack: StackItem[] = [
 		),
 	},
 	{
-		name: 'Java',
+		name: 'React',
 		icon: (
-			<LiaJava
-				size={21}
-				className="-mt-1 inline-block text-text-meta-lite ml-1"
+			<FaReact
+				size={16}
+				strokeWidth={1.5}
+				className="inline-block text-text-meta-lite ml-1.25 -mt-0.5"
 			/>
 		),
 	},
 	{
-		name: 'Spring',
+		name: 'Next.js',
 		icon: (
-			<SiSpring
-				size={13.5}
-				className="inline-block text-text-meta-lite ml-1.5 -mt-0.5"
+			<TbBrandNextjs
+				size={18}
+				strokeWidth={1.5}
+				className="inline-block text-text-meta-lite ml-1.25 -mt-0.5"
 			/>
 		),
 	},
